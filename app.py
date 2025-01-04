522824fdbff3c61fa2a99295efd9a772e891092f
sakshi jha <sakshijha446@gmail.com>
2025-01-04 14:42:17 +0530

    Waitress setup

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import mysql.connector
 import pandas as pd
+from waitress import serve
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
